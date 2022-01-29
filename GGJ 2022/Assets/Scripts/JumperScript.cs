844075a0ab49a02c59646c0a3bb1842c5486424e
f <edinkarakurt99@gmail.com>
2022-01-29 12:29:37 +0100

    Added Jumper Level Aspect

--- GGJ 2022/Assets/Scripts/JumperScript.cs
+++ GGJ 2022/Assets/Scripts/JumperScript.cs
@@ -4,34 +4,13 @@ using UnityEngine;
 
 public class JumperScript : MonoBehaviour
 {
-    //GameObject go1 = new GameObject();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void jump()
-    {
-        //print("sick");
-    }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            //Rigidbody rb = collision.GetComponent<Rigidbody>();
-            //rb.AddExplosionForce(10.0f, 5.0f, 1.0f, 3.0F);
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.up * 15f, ForceMode2D.Impulse);
         }
-
-
     }
 }
 
