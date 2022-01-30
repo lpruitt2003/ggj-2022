642831571634ca485d3769be28bd285691ec651e
f <edinkarakurt99@gmail.com>
2022-01-30 06:40:16 +0100

    Pipepification

--- GGJ 2022/Assets/Scripts/JumperScript.cs
+++ GGJ 2022/Assets/Scripts/JumperScript.cs
@@ -14,10 +14,16 @@ Edin Karakurt
 =====================================*/
 public class JumperScript : MonoBehaviour
 {
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            anim.Play("Boing");
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
             rb.AddForce(transform.up * 15f, ForceMode2D.Impulse);
         }
