e219d7eb3389b898021c8cae691bd899c81666f7
Pruitt, Logan James <lpruitt341@gmail.com>
2025-03-27 23:33:33 -0400

    Immunity powerup implemented

--- GGJ 2022/Assets/Scripts/PowerupApple.cs
+++ GGJ 2022/Assets/Scripts/PowerupApple.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class PowerupApple : MonoBehaviour
 {
-
-    // when either player 1 or player 2 collides with the apple, the player will be immune to the spikes for a short period of time
-
     public float immunityTime = 5f;
     public GameObject player1;
     public GameObject player2;
@@ -21,17 +18,24 @@ public class PowerupApple : MonoBehaviour
 
     IEnumerator Immunity(GameObject player)
     {
+        Debug.Log(player.name + " is now immune.");
+
         if (player.CompareTag("Player1"))
         {
             player.GetComponent<DeathLogic>().isImmune = true;
+            GetComponent<SpriteRenderer>().enabled = false;
         }
         else if (player.CompareTag("Player2"))
         {
             player.GetComponent<DeathLogic>().isImmune = true;
+            GetComponent<SpriteRenderer>().enabled = false;
         }
 
+        // after a certain amount of time, the player will no longer be immune to the spikes
         yield return new WaitForSeconds(immunityTime);
 
+        Debug.Log(player.name + "'s immunity has worn off.");
+
         if (player.CompareTag("Player1"))
         {
             player.GetComponent<DeathLogic>().isImmune = false;
@@ -40,5 +44,12 @@ public class PowerupApple : MonoBehaviour
         {
             player.GetComponent<DeathLogic>().isImmune = false;
         }
+
+        DestroyApple();
+    }
+
+    void DestroyApple()
+    {
+        Destroy(gameObject);
     }
 }
